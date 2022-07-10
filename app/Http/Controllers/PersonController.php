ae886c04b57ae69f2b735b8bf8eb1c94b207a43b
3509nkf <nagokaitofabian@gmail.com>
2022-07-11 00:15:53 +0900

    app

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Person;
+use App\Models\Person;
 
 class PersonController extends Controller
 {
     public function index(Request $request)
     {
      	$items = Person::all();
-        return view('person.index',['items' => $items]);
+        return view('person.index', ['items' => $items]);
+    }
+
+    public function search(Request $request)
+    {
+     	$item = Person::nameEqual($request->input)->first();
+        $param = ['input' => $request->input, 'item' => $item];
+        return view('person.find', $param);
+    }
+
+    public function find(Request $request) 
+    {
+     	return view('person.find',['input' => '']);
     }
 }
